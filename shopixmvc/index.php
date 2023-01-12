91cada4a83963c0d9d5ad17c4a4e7dc0b7dcbcf7
Y. Senel <94597933+yusuf57rl@users.noreply.github.com>
2023-01-12 13:13:38 +0100

    jadva

--- shopixmvc/index.php
+++ shopixmvc/index.php
@@ -1,29 +1,35 @@
 <?php
+
 declare(strict_types=1);
+
+use App\Core\Container;
+use App\Core\View;
 
 require __DIR__ . "/vendor/autoload.php";
 
-$container = new \App\Core\Container();
+$container = new Container();
 
 
 $dependencyprovider = new \App\Core\DependencyProvider();
 $dependencyprovider->provide($container);
 
 $provider = new \App\Core\ControllerProvider();
-$page = $_GET["page"] ?? "";
+$page = $_GET['page'] ?? '';
+
+$smarty = new Smarty();
 
 $controller = new \App\Controller\CategoryController($container);
-
 foreach ($provider->getList() as $key => $controllerClass) {
     if ($key === $page) {
         $controllerCheck = new $controllerClass($container);
-        if($controllerCheck instanceof \App\Controller\ControllerInterface){
+        if ($controllerCheck instanceof \App\Controller\ControllerInterface) {
             $controller = $controllerCheck;
+
             break;
         }
     }
 }
 
-
 $controller->load();
-$this->view->display();
+$container->get(View::class)->display();
+var_dump($container->get(View::class), new View(new Smarty()));
--- shopixmvc/template/Error.tpl
+++ shopixmvc/template/Error.tpl
@@ -1,1 +1,1 @@
-<h1>Page not found | 404</h1>
+<h1>Page not found | 404 !</h1>
